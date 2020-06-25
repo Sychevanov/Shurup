a6b02dfa9b2a52b0a2492eacde3ec9637d69a60f
roman <roman.sychevanov@gmail.com>
2020-06-25 17:27:53 +0500

    25

--- oop/student.py
+++ oop/student.py
@@ -3,8 +3,8 @@
 # опредлить у каждого сделать упражение, мальчики сделали 25 отжиманий, девочки 20 приседаний и сделать вывод,
 #  посчитать сколько сделали больше 20 и вывести кто сдал
 from abc import ABCMeta, abstractmethod
-from random import random
-sudent_mass = []
+import random
+
 
 class Student(metaclass=ABCMeta):
     @abstractmethod
@@ -13,36 +13,68 @@ class Student(metaclass=ABCMeta):
 
 class Man(Student):
     def zcht(self):
-        x = random.randint(0,100)
-        #print(f'Я отжался {x} раз')
+        x = random.randint(0,50)
+        print(f'Я отжался {x} раз')
         return x
         
 class Woman(Student):
     def zcht(self):
-        x = random.randint(0,100)
-        #print(f'Я присела {x} раз')
+        x = random.randint(0,50)
+        print(f'Я присела {x} раз')
         return x
+
 def manORwoman():
-    boy = Man() 
-    student_mass = []
+    boy = Man()
     girl = Woman()
+    student_mass = []    
     for i in range(0,21):
         x = random.randint(0,1)
         if x == 1:
             student_mass.append(boy)
         else:
             student_mass.append(girl)
+        
     return student_mass
 
+#a = [x = random.randint(0,1) for i in range(0,21) if x == 1 a.append(boy) else a.append(girl) ] думал может можно как-то так сделать
+#a = [random.randint(0,1) for i in range(0,21)]
+#b = [boy if a[i]==1 else girl for i in a]
+
 def zcht_fx(student_mass):
+    x = 0
     for i in range(0,21):
         if student_mass[i].zcht() > 20:
-            print('Экзамен сдан')
+            x = x + 1
+            print(f'Ученик номер {i}, Экзамен сдан\n')
         else:
-            print('Эзкамен не сдан')
+            print(f'Ученик номер {i}, Эзкамен не сдан \n')
+    return x
 
-manORwoman()
 
-#создать функцию для набора учеников рандомнго мальчики девочки(попробовать генераторы)
-#создать фкнцию перебора массива  вызовом метода, с проверкой кто сдал экзамен и сразу счетсик кто сдал
-#вывести сколько людей сдали экзамен
+
+
+
+
+def generator():    
+    boy = Man()
+    girl = Woman()
+    c = [boy if random.randint(0,1) == 1 else girl for i in range(0,21)]
+    x = 0
+    for i in range(0,21): #можно ли пробежаться по массиву без цтикла?
+        if c[i].zcht() > 20:
+            x = x + 1
+            print(f'Ученик номер {i}, Экзамен сдан\n')
+        else:
+            print(f'Ученик номер {i}, Эзкамен не сдан \n')
+    return x
+
+
+    
+
+print(zcht_fx(manORwoman()))
+#print(a)
+#print(b)
+#print(c)
+print(generator())
+
+    
