b7ed85c3f2d5890f7fe02bddb26d86bf768b6e85
roman <roman.sychevanov@gmail.com>
2020-12-01 18:07:53 +0500

    1.12

--- oop/arcanum_oop/locationMenu.py
+++ oop/arcanum_oop/locationMenu.py
@@ -1,7 +1,7 @@
 from menu import Menu
 from simpleMenuIItem import Simple_menu_item
 from menuItem import Menu_item
-from fx import foo
+from fx import foo, beginLocation1
 from newGame import newGame
 from picture_draw import zastavka, dirijable, virdgil, mashin
 from ArcanumMainMenu import ArcanumMainMenu
@@ -10,14 +10,17 @@ from Dialogs import dialogVirgil
 location_menu = Menu(title="Разрушенный дирижабль")
 location_menu.setStartupCommand(dirijable)
 location_menu.setStartupCommand(dialogVirgil)
-location_menu.addItems('Осмотреть кого-нибдуь',foo)
-somewhere_menu = location_menu.addSubMenu('Идти куда-нибудь')
+location_menu.setStartupCommand(beginLocation1)
+location_menu.addItems('Осмотреть мертвого человека',foo)
+somewhere_menu = location_menu.addSubMenu('Пройти вдоль разрушенного дирижабля')
 somewhere_menu.addItems('Сдлеать что-нибдуь где нибудь_1',foo)
 somewhere_menu.addItems('Сдлеать что-нибдуь где нибудь_2',foo)
-location_menu.addItems('Бой с кем-нибудь',foo)
-location_menu.addItems('Журнал',foo)
+location_menu.addItems('Волков',foo)
+
+#эту часть вынести, чтобы всегда было меню о персонаже
 infoChar_menu = location_menu.addSubMenu('О персонаже')
 infoChar_menu.addItems('Харакатерисики',foo)
+infoChar_menu.addItems('Журнал',foo)
 infoChar_menu.addItems('Сумка',foo)
 infoChar_menu.addItems('Инвертарь',foo)
 
